0077e916aacca8d91570b42ca19221f60d71a083
Ivan Peev <idobrinovv@gmail.com>
2020-10-21 12:41:10 +0300

    Add first software metrics example

---
+++ SoftwareMetrics_1/SoftwareMetrics_1/Exercises.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace SoftwareMetrics_1
+{
+    class Exercises
+    {
+        static int SumPositive(int a, int b)
+        {
+            if (a <= 0) throw new ArgumentException("'a' should be positive!", "a");
+            if (b <= 0) throw new ArgumentException("'b' should be positive!", "b");
+            return a + b;
+        }
+
+        static bool ContainsNegative(int a, int b, int c, int d)
+        {
+            if (a < 0 || b < 0 || c < 0 || d < 0) return false;
+            return true;
+        }
+
+        static bool ValidRGB(int r, int g, int b)
+        {
+            if (r < 0 || r > 255) return false;
+            if (g < 0 || g > 255) return false;
+            if (b < 0 || b > 255) return false;
+            return true;
+        }
+
+        static void DoSomeWork(int i)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Do some work, step 1");
+            }
+            Console.WriteLine("Other work...");
+
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.WriteLine($"Routine {j}");
+            }
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Do some work, step 2");
+            }
+
+            int k = 0;
+            while (k < 10)
+            {
+                Console.WriteLine(k);
+                k++;
+            }
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Do some work, step 3");
+            }
+
+            Console.WriteLine("Ready to go!");
+        }
+
+        static string Add(string number1, string number2)
+        {
+            int decNumber1;
+            try
+            {
+                decNumber1 = Convert.ToInt32(number1, 8);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            int decNumber2;
+            try
+            {
+                decNumber2 = Convert.ToInt32(number2, 8);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            int decResult = decNumber1 + decNumber2;
+            return Convert.ToString(decResult, 8);
+        }
+    }
+}
---
+++ SoftwareMetrics_1/SoftwareMetrics_1/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text;
+
+namespace SoftwareMetrics_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ReadKey();
+        }
+
+        //CC: 1
+        //NPath: 
+        public static bool IsNumber(string n)
+        {
+            int i;
+            return int.TryParse(n, out i);
+        }
+
+        //CC: 2
+        //NPath: 
+        public static bool IsNumber2(string n)
+        {
+            int i;
+            bool parsed = int.TryParse(n, out i);
+            if (parsed)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //CC: 2
+        //NPath: 
+        public static bool IsNumber3(string n)
+        {
+            try
+            {
+                int.Parse(n);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //CC: 3
+        //NPath: 
+        public static void Work(string i)
+        {
+            int n = 0;
+            if (int.TryParse(i, out n))
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //Do work
+                }
+            }
+        }
+
+        //CC: 3
+        //NPath: 
+        public static void CompareNumbers(int x, int y)
+        {
+            if (x == y) Console.Write("Equal");
+            else if (x > y) Console.Write("x is bigger than y.");
+            else Console.Write("y is bigger than x.");
+        }
+
+        //CC: 5
+        //NPath: 
+        public static void CompareNumbers2(string x, string y)
+        {
+            int i, j;
+            if (int.TryParse(x, out i) && int.TryParse(y, out j))
+            {
+                if (i == j) Console.Write("Equal");
+                else if (i > j) Console.Write("x is bigger than y.");
+                else Console.Write("y is bigger than x.");
+            }
+            else
+            {
+                Console.WriteLine("Inputs aren't numbers");
+            }
+        }
+
+        //CC: 12
+        //NPath: 
+        public static void TriangleType(int a, int b, int c)
+        {
+            string type = "разностранен";
+            if (a == b || b == c || c == a)
+            {
+                type = "равнобедрен";
+            }
+            if (a == b && a == c)
+            {
+                type = "равностранен";
+            }
+            if (a > b + c || b > a + c || c > a + b)
+            {
+                type = "Не съществува";
+            }
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                type = "Невалидни данни";
+            }
+            Console.WriteLine($"Триъгълник със страни {a}, {b}, {c} е {type}.");
+        }
+        
+        //CC: 4
+        //NPath: 
+        public static void GetMedal(int position)
+        {
+            string medal = null;
+            switch (position)
+            {
+                case 1:
+                    medal = "Gold";
+                break;
+                case 2:
+                    medal = "Silver";
+                    break;
+                case 3:
+                    medal = "Bronze";
+                    break;
+                default:
+                    medal = "Sorry, try again next time!";
+                break;
+            }
+            Console.WriteLine(medal);
+        }
+
+        //CC: 4
+        //NPath: 
+        public static void GetMedal2(int position)
+        {
+            string medal = null;
+            if (position == 1)
+            {
+                medal = "Gold";
+            }
+            else if (position == 2)
+            {
+                medal = "Silver";
+            }
+            else if (position == 3)
+            {
+                medal = "Bronze";
+            }
+            else
+            {
+                medal = "Sorry, try again next time!";
+            }
+            Console.WriteLine(medal);
+        }
+    }
+}
